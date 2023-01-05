fb936ace53c5674a7b5f4f817e56141719dd8914
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-05 23:20:44 +0000

    added further styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "../scss/index.scss";
 
 const App = () => {
+
+    const [password, setPassword] = useState('');
+    const [passwordLength, setPasswordLength] = useState(65);
+    const [includeUppercase, setIncludeUppercase] = useState(false);
+    const [includeLowercase, setIncludeLowercase] = useState(false);
+    const [includeNumbers, setIncludeNumbers] = useState(false);
+    const [includeSymbols, setIncludeSymbols] = useState(false);
+
     return (
         <div className="Container">
         <h1 className="PasswordGeneratorHeader">Password Generator</h1>
@@ -13,7 +22,7 @@ const App = () => {
             <div className="Forms">
                 <div className="FormGroup">
                     <label htmlFor="PasswordStrength">Password Strength</label>
-                    <label for="PasswordStrengthSlider" id="SliderStrengthText">8</label>
+                    <label htmlFor="PasswordStrengthSlider" id="SliderStrengthText">8</label>
                     <input type="range" id="PasswordStrengthSlider" name="PasswordStrengthSlider" max="65" min="8"/>
                 </div>
                 <div className="FormGroup">
