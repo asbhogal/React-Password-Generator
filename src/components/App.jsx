c1df37f614462aeaf0a7f21de496ad0b32f1ecbd
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-05 20:21:29 +0000

    populated with content and styling

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import "../scss/index.scss";
+
+const App = () => {
+    return (
+        <div className="Container">
+        <h1>Password Generator</h1>
+            <div className="PasswordGenerator">
+                <h2 className="PasswordGeneratorHeader">Password</h2>
+                <button className="CopyPasswordBtn">
+                    <i className="fa-solid fa-clipboard"></i>
+                </button>
+            </div>
+            <div className="Forms">
+                <div className="FormGroup">
+                    <label htmlFor="PasswordStrength">Password Strength</label>
+                    <label for="PasswordStrengthSlider" id="SliderStrengthText">8</label>
+                    <input type="range" id="PasswordStrengthSlider" name="PasswordStrengthSlider" max="65" min="8"/>
+                </div>
+                <div className="FormGroup">
+                    <label htmlFor="UpperCaseLetters">Uppercase Letters (A-Z)</label>
+                    <input type="checkbox" id="UpperCaseLettersCheck" name="UpperCaseLettersCheck"/>
+                </div>
+                <div className="FormGroup">
+                    <label htmlFor="LowerCaseLetters">Lowercase Letters (a-z)</label>
+                    <input type="checkbox" id="LowerCaseLettersCheck" name="LowerCaseLettersCheck"/>
+                </div>
+                <div className="FormGroup">
+                    <label htmlFor="LowerCaseLetters">Numbers (0-9)</label>
+                    <input type="checkbox" id="NumbersCheck" name="NumbersCheck"/>
+                </div>
+                <div className="FormGroup">
+                    <label htmlFor="LowerCaseLetters">Symbols</label>
+                    <input type="checkbox" id="SymbolsCheck" name="SymbolsCheck"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
